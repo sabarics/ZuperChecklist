4c37ecea790ae7d36a5dc35800af6b13634fea21
sabarics <sabarics10@gmail.com>
2020-04-24 18:34:29 +0530

    initial commit

---
+++ ZuperChecklist/ZuperChecklist/ZuperChecklist.h
@@ -0,0 +1,19 @@
+//
+//  ZuperChecklist.h
+//  ZuperChecklist
+//
+//  Created by Zuper on 24/04/20.
+//  Copyright © 2020 Zuper. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZuperChecklist.
+FOUNDATION_EXPORT double ZuperChecklistVersionNumber;
+
+//! Project version string for ZuperChecklist.
+FOUNDATION_EXPORT const unsigned char ZuperChecklistVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZuperChecklist/PublicHeader.h>
+
+
